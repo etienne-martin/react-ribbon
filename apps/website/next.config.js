f0a2864ed51b5f37d87b08f2574b5c987e6503ad
Etienne <>
2022-09-25 19:57:00 -0400

    fix next config

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   distDir: "./dist/.next",
-  basePath: process.env.WEBSITE_BASE_PATH ?? "/",
+  basePath: process.env.WEBSITE_BASE_PATH,
   eslint: {
     ignoreDuringBuilds: true,
   },
