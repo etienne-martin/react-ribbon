d6a3769691f95598a2457a7c1aea24202a48cc95
Etienne <>
2022-09-25 20:08:04 -0400

    set auth token

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,6 +21,8 @@ PACKAGE_VERSION=$(cat package.json \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
 
+npm config set //registry.npmjs.org/:_authToken="${NPM_AUTH_TOKEN}"
+
 if npm show "$PACKAGE_NAME" version | grep -w "$PACKAGE_VERSION" > /dev/null; then
   echo "Package $PACKAGE_NAME@$PACKAGE_VERSION already exists, publishing to @next"
   npm version "$PACKAGE_VERSION-next-$TIMESTAMP"
